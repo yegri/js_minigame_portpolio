162ce40f8f592b9cec2334ad6b5f5e0707b26822
yegri <exsilver305@gamil.com>
2022-10-03 04:25:09 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('i');
+const timer = document.querySelector('.timer');
+const count = document.querySelector('.counter');
+
+function startGame() {
+    // 1. 시작버튼이 정지버튼으로 바뀜
+    
+    // 2. 타이머 시작
+
+}
